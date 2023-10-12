1d9f0326016969cab4f1dd0c43be5457db7f6042
理塘最強伝説と絶兇の猛虎!純真丁一郎參上! <130117362+WeishanOoi@users.noreply.github.com>
2023-10-12 10:00:32 +0800

    Update sdmgr.py

--- mmocr/models/kie/extractors/sdmgr.py
+++ mmocr/models/kie/extractors/sdmgr.py
@@ -144,7 +144,23 @@ class SDMGR(BaseModel):
             [data_sample.gt_instances.bboxes for data_sample in data_samples])
         return self.kie_head.loss(x, data_samples)
 
-    def predict(self, inputs: torch.Tensor, data_samples: Sequence[KIEDataSample], **kwargs) -> List[KIEDataSample]:
+    def predict(self, inputs: torch.Tensor,
+                data_samples: Sequence[KIEDataSample],
+                **kwargs) -> List[KIEDataSample]:
+        """Predict results from a batch of inputs and data samples with post-
+        processing.
+        Args:
+            inputs (torch.Tensor): Input images of shape (N, C, H, W).
+                Typically these should be mean centered and std scaled.
+            data_samples (list[KIEDataSample]): A list of N datasamples,
+                containing meta information and gold annotations for each of
+                the images.
+
+        Returns:
+            List[KIEDataSample]: A list of datasamples of prediction results.
+            Results are stored in ``pred_instances.labels`` and
+            ``pred_instances.edge_labels``.
+        """
         x = self.extract_feat(
             inputs,
             [data_sample.gt_instances.bboxes for data_sample in data_samples])
@@ -158,19 +174,18 @@ class SDMGR(BaseModel):
                     combined_entities[entity_type] = entity
                 else:
                     combined_entities[entity_type] += ' ' + entity
-    
+
             # Create a formatted entity, e.g., 'passenger name: Jane Doe'
             formatted_entity = {
                 'label': 'passenger name',
                 'text': combined_entities.get('Person', '')
             }
-    
+
             # Update the prediction with the formatted entity
             prediction.labels = [formatted_entity]
             prediction.entity_types = ['Person']
 
-    return predictions
-
+        return predictions
 
     def _forward(self, inputs: torch.Tensor,
                  data_samples: Sequence[KIEDataSample],
